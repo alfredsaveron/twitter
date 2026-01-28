f524711a682131b6ddc6808bb59e7ceaf9df5d95
Ege <57676676+alfredsaveron@users.noreply.github.com>
2026-01-28 01:26:07 +0100

    component with metadata

---
+++ page.tsx
@@ -0,0 +1,18 @@
+import { TwitterProfile } from '@/components/sections/twitter-profile'
+import { twitterConfig } from '@/config/twitter'
+
+export const metadata = {
+    title: 'Twitter',
+    description: 'My Twitter profile',
+}
+
+export default function TwitterPage() {
+    return (
+        <>
+            <Hero title="Twitter" />
+            <Container type="section" className="pb-16">
+                <TwitterProfile config={twitterConfig} />
+            </Container>
+        </>
+    )
+}
