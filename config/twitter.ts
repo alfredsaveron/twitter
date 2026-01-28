5a2b55b33a264835f7facffaab8fdcc48dea091a
Ege <57676676+alfredsaveron@users.noreply.github.com>
2026-01-28 01:28:24 +0100

    configuration file

---
+++ config/twitter.ts
@@ -0,0 +1,23 @@
+export const twitterConfig = {
+    // info
+    displayName: 'e',
+    username: 'alfredsaveron',
+    bio: '', // leave it like '' for no info.
+    isPrivate: true,
+    isVerified: false,
+
+    category: 'Science & Technology',
+    location: 'Wien, Österreich',
+    website: 'ege.rip',
+    joinedDate: 'September 2019',
+
+    following: 21,
+    followers: 34,
+
+    avatarUrl: '/design/twitter-avatar.png',
+    bannerUrl: '/design/twitter-banner.png',
+
+    profileUrl: 'https://x.com/twitter',
+}
+
+export type TwitterConfig = typeof twitterConfig
